a9214ca0694af7dd5f262cf1fe21011d4955d530
Josemari Feliciano <josemari.feliciano@gmail.com>
2019-03-29 12:27:12 -0400

    updated files

--- logistic/solution_for862.R
+++ logistic/solution_for862.R
@@ -8,3 +8,15 @@ library(readr)
 taxo_data <- read_table("8_6_2_Taxo.txt", 
                         col_names = c("dose","gcf_use","neut_cases","treat_cases"))
 
+write.csv(taxo_data, file="formatted_taxo_data.csv")
+
+
+web_link_data <- "https://raw.githubusercontent.com/neonseri/spring2019_yale_bis505/master/nonparametric/modified_clothwaste.txt"
+cloth_waste_df <- read.table(file=web_link_data, header=TRUE)
+
+https://raw.githubusercontent.com/neonseri/spring2019_yale_bis505/master/logistic/formatted_taxo_data.csv
+
+
+
+web_link_data <- "https://raw.githubusercontent.com/neonseri/spring2019_yale_bis505/master/logistic/formatted_taxo_data.csv"
+taxo_data <- read.table(file=web_link_data, header=TRUE)
